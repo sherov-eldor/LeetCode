d12f52594aa4942f18de7d40ffbd7f9f62be6a8e
Eldor <eldor.sherov.97@inbox.ru>
2024-01-19 15:14:52 +0300

    added second solution

--- src/Python/easy/array/problem1313.py
+++ src/Python/easy/array/problem1313.py
@@ -1,5 +1,18 @@
 from typing import List
 
+# First solution
+
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        answer = []
+        for i in range(0,len(nums), 2):
+            arr = [nums[i], nums[i + 1]]
+            for k in range(0, arr[0]):
+                answer.append(arr[1])
+        return answer
+
+# Second solution
+
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
         answer = []
@@ -7,8 +20,6 @@ class Solution:
             arr = [nums[i], nums[i + 1]]
             answer.extend([arr[1]] * arr[0])
         return answer
-
-
 
 nums = [1,2,3,4]
 
