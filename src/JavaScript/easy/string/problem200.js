e99edcb40a7b26dade17f26cfb55e2bae7f8facd
Eldor <eldor.sherov.97@inbox.ru>
2024-01-24 14:41:43 +0300

    Revert "https://leetcode.com/problems/reverse-prefix-of-word/"
    This reverts commit d5a1f19fe72afb09e0328630de53d856e8e13e2d.

--- src/JavaScript/easy/string/problem200.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} word
- * @param {character} ch
- * @return {string}
- */
-var reversePrefix = function (word, ch) {
-    let index = word.indexOf(ch)
-    return word.slice(0, index + 1).split("").reverse().join("") + word.slice(index + 1, word.length)
-};
