c1384b345c0e41be5ee91e3e813c2121be483e1a
Eldor <eldor.sherov.97@inbox.ru>
2024-01-25 10:40:39 +0300

    https://leetcode.com/problems/find-the-maximum-achievable-number

---
+++ src/JavaScript/easy/math/problem2769.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function (num, t) {
+    return num + t + t
+};
+
+let num = 4, t = 1
+
+theMaximumAchievableX(num, t)
