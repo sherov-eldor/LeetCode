512772a36ed35951d500121ab0c995eb9188d276
Eldor <eldor.sherov.97@inbox.ru>
2024-01-25 10:36:46 +0300

    https://leetcode.com/problems/find-the-maximum-achievable-number/

---
+++ src/Python/easy/math/problem2769.py
@@ -0,0 +1,10 @@
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + t + t
+
+
+num = 4; t = 1
+
+
+solution = Solution()
+solution.theMaximumAchievableX(num, t)
