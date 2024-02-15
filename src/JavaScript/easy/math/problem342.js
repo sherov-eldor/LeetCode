8137eafcdf35c56b308822d73df82ccb5a8a3f7a
Eldor <eldor.sherov.97@inbox.ru>
2024-02-15 17:22:56 +0300

    https://leetcode.com/problems/power-of-four

---
+++ src/JavaScript/easy/math/problem342.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    console.log(Math.log2(n));
+    console.log(Number.isInteger(Math.log2(n)/2));
+};
+
+let n = 16
+// let n = 1
+// let n = 5
+// let n = 2
+
+isPowerOfFour(n)
