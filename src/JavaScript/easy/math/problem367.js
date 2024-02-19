854db113b35c8916b1046f9148db5d683694d5e2
Eldor <eldor.sherov.97@inbox.ru>
2024-02-19 11:30:56 +0300

    https://leetcode.com/problems/valid-perfect-square

---
+++ src/JavaScript/easy/math/problem367.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    return Number.isInteger(Math.sqrt(num))
+};
+
+
+// let num = 16
+let num = 14
+
+isPerfectSquare(num)
