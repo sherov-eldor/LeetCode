cd2b4bdbd71606fe52de7a3e463a7e674a35474c
Eldor <eldor.sherov.97@inbox.ru>
2024-03-07 12:23:45 +0300

    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string

---
+++ src/JavaScript/easy/string/problem28.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
+
+let haystack = "sadbutsad", needle = "sad"
+
+strStr(haystack, needle)
